b73de82e5c908adb4a73e9627529e80486e9c23a
sabbir20201 <sabbirsarkar674@gmail.com>
2024-05-18 18:46:19 +0600

    INSTALLED some pakeges

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+// perser
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 20;
+  // res.send('Hello World!')
+
+  res.send(a);
+});
+export default app;
+console.log(process.cwd());
